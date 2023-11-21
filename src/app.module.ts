2fde1b7b67fca6fa48b20cf5adf03dcbc0d4f399
DavidTMaciel <davidmacieljr122@gmail.com>
2023-11-21 18:51:30 -0300

    module the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { CarsModule } from './cars/cars.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, CarsModule],
+  imports: [UsersModule, CarsModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
